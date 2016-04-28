8d439c704fc0447fa9d7f91f3930bdca6ad3c81c
Kyle Meenehan <kylemeenehan@gmail.com>
2016-04-28 17:38:26 +0200

    added basic file for hilux horizontal parallax

--- app/app/app.js
+++ app/app/app.js
@@ -2,5 +2,7 @@ angular.module('PortfolioApp', ['ngRoute'])
 .config(function($routeProvider){
 	$routeProvider.when('/',{
 		templateUrl: "app/views/home.html"
+	}).when('/hilux-parallax',{
+		templateUrl: "app/views/hilux_parallax.html"
 	});
 });
